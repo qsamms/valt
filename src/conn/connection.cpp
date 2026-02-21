9a86465824548c4a5c31e7e50101073dd6f353ba
Quinn Samms <qsamms11@gmail.com>
2026-02-20 22:25:33 -0800

    separate connection code

---
+++ src/conn/connection.cpp
@@ -0,0 +1,63 @@
+#include "connection.h"
+
+Connection::Connection(int client_fd)
+    : fd(client_fd),
+      ssl(nullptr),
+      authenticated(false),
+      mode(SessionMode::DB),
+      read_watcher(std::make_unique<ev_io>()),
+      write_watcher(std::make_unique<ev_io>()),
+      write_state(std::make_unique<WatcherState>()),
+      read_state(std::make_unique<WatcherState>()) {
+}
+
+Connection::Connection(int client_fd, SSL* ssl)
+    : fd(client_fd),
+      ssl(ssl),
+      authenticated(false),
+      mode(SessionMode::DB),
+      read_watcher(std::make_unique<ev_io>()),
+      write_watcher(std::make_unique<ev_io>()),
+      write_state(std::make_unique<WatcherState>()),
+      read_state(std::make_unique<WatcherState>()) {
+}
+
+Connection::~Connection() {
+    if (read_watcher) ev_io_stop(EV_DEFAULT, read_watcher.get());
+    if (write_watcher) ev_io_stop(EV_DEFAULT, write_watcher.get());
+
+    if (ssl != nullptr) {
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+    }
+    close(fd);
+}
+
+void Connection::add_to_queue(const std::string& s) {
+    write_queue.push_back(std::move(s));
+}
+
+void Connection::reset_state(WatcherState* state) {
+    state->buffer.clear();
+    state->total_bytes = 0;
+}
+
+void Connection::start_write_watcher(void (*callback)(struct ev_loop*, ev_io*, int)) {
+    ev_io_stop(EV_DEFAULT, write_watcher.get());
+    ev_io_init(write_watcher.get(), callback, fd, EV_WRITE);
+    ev_io_start(EV_DEFAULT, write_watcher.get());
+}
+
+void Connection::start_read_watcher(void (*callback)(struct ev_loop*, ev_io*, int)) {
+    ev_io_stop(EV_DEFAULT, read_watcher.get());
+    ev_io_init(read_watcher.get(), callback, fd, EV_READ);
+    ev_io_start(EV_DEFAULT, read_watcher.get());
+}
+
+void Connection::stop_write_watcher() {
+    ev_io_stop(EV_DEFAULT, write_watcher.get());
+}
+
+void Connection::stop_read_watcher() {
+    ev_io_stop(EV_DEFAULT, read_watcher.get());
+}
--- src/conn/connection.h
+++ src/conn/connection.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <ev.h>
-#include <net/network_layer.h>
 #include <openssl/ssl.h>
 #include <types/types.h>
 
@@ -27,58 +26,16 @@ struct Connection {
     std::unique_ptr<WatcherState> read_state;
     std::deque<std::string> write_queue;
 
-    Connection(int client_fd)
-        : fd(client_fd),
-          ssl(nullptr),
-          authenticated(false),
-          mode(SessionMode::DB),
-          read_watcher(std::make_unique<ev_io>()),
-          write_watcher(std::make_unique<ev_io>()),
-          write_state(std::make_unique<WatcherState>()),
-          read_state(std::make_unique<WatcherState>()) {}
-
-    Connection(int client_fd, SSL* ssl)
-        : fd(client_fd),
-          ssl(ssl),
-          authenticated(false),
-          mode(SessionMode::DB),
-          read_watcher(std::make_unique<ev_io>()),
-          write_watcher(std::make_unique<ev_io>()),
-          write_state(std::make_unique<WatcherState>()),
-          read_state(std::make_unique<WatcherState>()) {}
-
+    Connection(int client_fd);
+    Connection(int client_fd, SSL* ssl);
     Connection(Connection&&) noexcept = default;
     Connection& operator=(Connection&&) noexcept = default;
+    ~Connection();
 
-    ~Connection() {
-        if (read_watcher) ev_io_stop(EV_DEFAULT, read_watcher.get());
-        if (write_watcher) ev_io_stop(EV_DEFAULT, write_watcher.get());
-
-        if (ssl != nullptr) {
-            SSL_shutdown(ssl);
-            SSL_free(ssl);
-        }
-        close(fd);
-    }
-
-    void add_to_queue(const std::string& s) { write_queue.push_back(std::move(s)); }
-
-    void reset_state(WatcherState* state) {
-        state->buffer.clear();
-        state->total_bytes = 0;
-    }
-
-    void start_write_watcher(void (*callback)(struct ev_loop*, ev_io*, int)) {
-        ev_io_stop(EV_DEFAULT, write_watcher.get());
-        ev_io_init(write_watcher.get(), callback, fd, EV_WRITE);
-        ev_io_start(EV_DEFAULT, write_watcher.get());
-    }
-    void start_read_watcher(void (*callback)(struct ev_loop*, ev_io*, int)) {
-        ev_io_stop(EV_DEFAULT, read_watcher.get());
-        ev_io_init(read_watcher.get(), callback, fd, EV_READ);
-        ev_io_start(EV_DEFAULT, read_watcher.get());
-    }
-
-    void stop_write_watcher() { ev_io_stop(EV_DEFAULT, write_watcher.get()); }
-    void stop_read_watcher() { ev_io_stop(EV_DEFAULT, read_watcher.get()); }
+    void add_to_queue(const std::string& s);
+    void reset_state(WatcherState* state);
+    void start_write_watcher(void (*callback)(struct ev_loop*, ev_io*, int));
+    void start_read_watcher(void (*callback)(struct ev_loop*, ev_io*, int));
+    void stop_write_watcher();
+    void stop_read_watcher();
 };
