4097b53bbda765b7e8e473e619687e881e69c31e
Quinn Samms <qsamms11@gmail.com>
2026-02-20 22:16:11 -0800

    cleanup

--- src/net/network_layer.cpp
+++ src/net/network_layer.cpp
@@ -57,131 +57,131 @@ void pubsub_write_cb(EV_P_ ev_io* watcher, int revents) {
 }
 
 void client_write_cb(EV_P_ ev_io* watcher, int revents) {
-    if (revents & EV_WRITE) {
-        auto& conns = get_connections();
-        if (!conns.contains(watcher->fd)) {
-            ev_io_stop(EV_A_ watcher);
-            delete watcher;
+    if (!(revents & EV_WRITE)) return;
+
+    auto& conns = get_connections();
+    if (!conns.contains(watcher->fd)) {
+        ev_io_stop(EV_A_ watcher);
+        delete watcher;
+        return;
+    }
+    Connection* c = conns.at(watcher->fd).get();
+    SSL* ssl = c->ssl;
+    WatcherState* state = c->write_state.get();
+
+    int bytes_sent = 0;
+    if (ssl != nullptr) {
+        bytes_sent = SSL_write(ssl, state->buffer.c_str(), state->buffer.size());
+        if (bytes_sent <= 0) {
+            int err = SSL_get_error(ssl, bytes_sent);
+            if (err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) return;
+            conns.erase(watcher->fd);
             return;
         }
-        Connection* c = conns.at(watcher->fd).get();
-        SSL* ssl = c->ssl;
-        WatcherState* state = c->write_state.get();
-
-        int bytes_sent = 0;
-        if (ssl != nullptr) {
-            bytes_sent = SSL_write(ssl, state->buffer.c_str(), state->buffer.size());
-            if (bytes_sent <= 0) {
-                int err = SSL_get_error(ssl, bytes_sent);
-                if (err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) return;
-                conns.erase(watcher->fd);
-                return;
-            }
-        } else {
-            bytes_sent = send(watcher->fd, state->buffer.c_str(), state->buffer.size(), 0);
-            if (bytes_sent <= 0) {
-                if (errno == EAGAIN || errno == EWOULDBLOCK) return;
-                conns.erase(watcher->fd);
-                return;
-            }
+    } else {
+        bytes_sent = send(watcher->fd, state->buffer.c_str(), state->buffer.size(), 0);
+        if (bytes_sent <= 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) return;
+            conns.erase(watcher->fd);
+            return;
         }
+    }
 
-        state->buffer = state->buffer.substr(bytes_sent);
-        if (state->buffer.empty()) {
-            c->stop_write_watcher();
-        }
+    state->buffer = state->buffer.substr(bytes_sent);
+    if (state->buffer.empty()) {
+        c->stop_write_watcher();
     }
 }
 
 void client_read_cb(EV_P_ ev_io* watcher, int revents) {
-    char buffer[1024];
+    if (!(revents & EV_READ)) return;
 
-    if (revents & EV_READ) {
-        auto& conns = get_connections();
-        if (!conns.contains(watcher->fd)) {
-            ev_io_stop(EV_A_ watcher);
-            delete watcher;
+    auto& conns = get_connections();
+    if (!conns.contains(watcher->fd)) {
+        ev_io_stop(EV_A_ watcher);
+        delete watcher;
+        return;
+    }
+    Connection* c = conns.at(watcher->fd).get();
+    WatcherState* state = c->read_state.get();
+    SSL* ssl = c->ssl;
+
+    char buffer[1024];
+    int bytes_read = 0;
+
+    if (ssl != nullptr) {
+        bytes_read = SSL_read(ssl, buffer, sizeof(buffer));
+        if (bytes_read <= 0) {
+            int err = SSL_get_error(ssl, bytes_read);
+            if (err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) return;
+            conns.erase(watcher->fd);
             return;
         }
-        Connection* c = conns.at(watcher->fd).get();
-        WatcherState* state = c->read_state.get();
-        SSL* ssl = c->ssl;
-
-        int bytes_read = 0;
-        if (ssl != nullptr) {
-            bytes_read = SSL_read(ssl, buffer, sizeof(buffer));
-            if (bytes_read <= 0) {
-                int err = SSL_get_error(ssl, bytes_read);
-                if (err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) return;
-                conns.erase(watcher->fd);
-                return;
-            }
-        } else {
-            bytes_read = recv(watcher->fd, buffer, sizeof(buffer), 0);
-            if (bytes_read <= 0) {
-                if (errno == EAGAIN || errno == EWOULDBLOCK) return;
-                conns.erase(watcher->fd);
-                return;
-            }
+    } else {
+        bytes_read = recv(watcher->fd, buffer, sizeof(buffer), 0);
+        if (bytes_read <= 0) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) return;
+            conns.erase(watcher->fd);
+            return;
         }
+    }
 
-        bool first_read = state->buffer.size() == 0;
-        if (first_read) {
-            if (bytes_read >= 4) {
+    bool first_read = state->buffer.size() == 0;
+    if (first_read) {
+        if (bytes_read >= 4) {
+            uint32_t message_length = 0;
+            memcpy(reinterpret_cast<char*>(&message_length), buffer, 4);
+            message_length = ntohl(message_length);
+            std::string message = std::string(buffer + 4, bytes_read - 4);
+            state->buffer = message;
+            state->total_bytes = message_length;
+        } else {
+            state->buffer = std::string(buffer, bytes_read);
+            state->total_bytes = 0;
+        }
+    } else {
+        std::string& read_buffer = state->buffer;
+
+        if (state->total_bytes == 0) {
+            // We still don't know the data length
+            uint32_t total_bytes_so_far = read_buffer.size() + bytes_read;
+            if (total_bytes_so_far >= 4) {
                 uint32_t message_length = 0;
-                memcpy(reinterpret_cast<char*>(&message_length), buffer, 4);
+                memcpy(reinterpret_cast<char*>(&message_length), read_buffer.c_str(),
+                       read_buffer.size());
+                memcpy(reinterpret_cast<char*>(&message_length) + read_buffer.size(), buffer,
+                       4 - read_buffer.size());
                 message_length = ntohl(message_length);
-                std::string message = std::string(buffer + 4, bytes_read - 4);
-                state->buffer = message;
                 state->total_bytes = message_length;
+
+                uint32_t message_bytes = total_bytes_so_far - 4;
+                if (message_bytes > 0) {
+                    uint32_t offset = bytes_read - message_bytes;
+                    state->buffer = std::string(buffer + offset, bytes_read - offset);
+                }
             } else {
-                state->buffer = std::string(buffer, bytes_read);
-                state->total_bytes = 0;
+                state->buffer = std::string(read_buffer + std::string(buffer, bytes_read));
             }
         } else {
-            std::string& read_buffer = state->buffer;
-
-            if (state->total_bytes == 0) {
-                // We still don't know the data length
-                uint32_t total_bytes_so_far = read_buffer.size() + bytes_read;
-                if (total_bytes_so_far >= 4) {
-                    uint32_t message_length = 0;
-                    memcpy(reinterpret_cast<char*>(&message_length), read_buffer.c_str(),
-                           read_buffer.size());
-                    memcpy(reinterpret_cast<char*>(&message_length) + read_buffer.size(), buffer,
-                           4 - read_buffer.size());
-                    message_length = ntohl(message_length);
-                    state->total_bytes = message_length;
-
-                    uint32_t message_bytes = total_bytes_so_far - 4;
-                    if (message_bytes > 0) {
-                        uint32_t offset = bytes_read - message_bytes;
-                        state->buffer = std::string(buffer + offset, bytes_read - offset);
-                    }
-                } else {
-                    state->buffer = std::string(read_buffer + std::string(buffer, bytes_read));
-                }
+            // We know the data length we just haven't received it all yet
+            if (read_buffer.size() + bytes_read >= state->total_bytes) {
+                state->buffer = std::string(
+                    read_buffer + std::string(buffer, state->total_bytes - read_buffer.size()));
             } else {
-                // We know the data length we just haven't received it all yet
-                if (read_buffer.size() + bytes_read >= state->total_bytes) {
-                    state->buffer = std::string(
-                        read_buffer + std::string(buffer, state->total_bytes - read_buffer.size()));
-                } else {
-                    state->buffer = std::string(read_buffer + std::string(buffer, bytes_read));
-                }
+                state->buffer = std::string(read_buffer + std::string(buffer, bytes_read));
             }
         }
+    }
 
-        if (state->buffer.size() == state->total_bytes) {
-            Valt& valt = Valt::getInstance();
-            spdlog::info("{}", state->buffer);
-            std::string response = valt.execute(state->buffer, watcher->fd);
-            WatcherState* write_state = c->write_state.get();
-            write_state->buffer = std::move(utils::length_prefixed(response));
-            write_state->total_bytes = 0;
-            c->start_write_watcher(client_write_cb);
-            c->reset_state(c->read_state.get());
-        }
+    if (state->buffer.size() == state->total_bytes) {
+        Valt& valt = Valt::getInstance();
+        spdlog::info("{}", state->buffer);
+        std::string response = valt.execute(state->buffer, watcher->fd);
+        WatcherState* write_state = c->write_state.get();
+        write_state->buffer = std::move(utils::length_prefixed(response));
+        write_state->total_bytes = 0;
+        c->start_write_watcher(client_write_cb);
+        c->reset_state(c->read_state.get());
     }
 }
 
@@ -218,35 +218,35 @@ void tls_handshake_cb(EV_P_ ev_io* watcher, int revents) {
 }
 
 void accept_connection_cb(EV_P_ ev_io* watcher, int revents) {
-    if (revents & EV_READ) {
-        struct sockaddr_in address;
-        socklen_t addrlen = sizeof(address);
-        int client_fd = accept(watcher->fd, (struct sockaddr*)&address, &addrlen);
-        if (client_fd == -1) {
-            spdlog::debug("Failed to accept connection");
-            return;
-        }
-        utils::set_nonblocking(client_fd);
+    if (!(revents & EV_READ)) return;
 
-        char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &address.sin_addr, client_ip, sizeof(client_ip));
-        int client_port = ntohs(address.sin_port);
-        spdlog::debug("Accepted connection from: {}", client_ip);
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+    int client_fd = accept(watcher->fd, (struct sockaddr*)&address, &addrlen);
+    if (client_fd == -1) {
+        spdlog::debug("Failed to accept connection");
+        return;
+    }
+    utils::set_nonblocking(client_fd);
 
-        auto& conns = get_connections();
-        SSL_CTX* ssl_ctx = static_cast<SSL_CTX*>(watcher->data);
-        if (ssl_ctx == nullptr) {
-            conns.try_emplace(client_fd, std::make_unique<Connection>(client_fd));
-            Connection* c = conns.at(client_fd).get();
-            c->start_read_watcher(client_read_cb);
-        } else {
-            SSL* ssl = SSL_new(ssl_ctx);
-            SSL_set_fd(ssl, client_fd);
-            SSL_set_accept_state(ssl);  // server mode
-            conns.try_emplace(client_fd, std::make_unique<Connection>(client_fd, ssl));
-            Connection* c = conns.at(client_fd).get();
-            c->start_read_watcher(tls_handshake_cb);
-        }
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &address.sin_addr, client_ip, sizeof(client_ip));
+    int client_port = ntohs(address.sin_port);
+    spdlog::debug("Accepted connection from: {}", client_ip);
+
+    auto& conns = get_connections();
+    SSL_CTX* ssl_ctx = static_cast<SSL_CTX*>(watcher->data);
+    if (ssl_ctx == nullptr) {
+        conns.try_emplace(client_fd, std::make_unique<Connection>(client_fd));
+        Connection* c = conns.at(client_fd).get();
+        c->start_read_watcher(client_read_cb);
+    } else {
+        SSL* ssl = SSL_new(ssl_ctx);
+        SSL_set_fd(ssl, client_fd);
+        SSL_set_accept_state(ssl);  // server mode
+        conns.try_emplace(client_fd, std::make_unique<Connection>(client_fd, ssl));
+        Connection* c = conns.at(client_fd).get();
+        c->start_read_watcher(tls_handshake_cb);
     }
 }
 
